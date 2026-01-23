b5e5d448590483b8215c977f6c506a6f9f58f0d8
Neka83 <n_neike@mail.ru>
2026-01-23 16:36:17 +0500

    Implement dynamic rule conditions evaluation (stage 2)

--- src/main/java/ru/starbank/recommendation_service/rules/service/RuleConditionEvaluatorImpl.java
+++ src/main/java/ru/starbank/recommendation_service/rules/service/RuleConditionEvaluatorImpl.java
@@ -10,8 +10,30 @@ public class RuleConditionEvaluatorImpl implements RuleConditionEvaluator {
 
     @Override
     public boolean evaluate(UUID userId, RuleConditionEntity condition) {
-        // ️ Минимальная реализация для приёма ДЗ
-        // логика расширяется в курсовой
-        return true;
+
+        boolean result;
+
+        switch (condition.getQuery()) {
+
+            case "ALWAYS_TRUE":
+                result = true;
+                break;
+
+            case "ALWAYS_FALSE":
+                result = false;
+                break;
+
+            case "USER_ID_STARTS_WITH":
+                result = userId.toString()
+                        .startsWith(condition.getArguments());
+                break;
+
+            default:
+                // неизвестное правило — считаем ложным
+                result = false;
+        }
+
+        // поддержка negate
+        return condition.isNegate() ? !result : result;
     }
 }
